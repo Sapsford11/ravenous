d594723f2ecadefc9b03afb072f822fe24e24aa7
Sapsford11 <craig.sapsford11@gmail.com>
2018-03-13 19:32:52 +0100

    Google Maps API added

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -11,9 +11,9 @@ class Business extends React.Component {
         <a href={this.props.business.url} target="_blank" rel="noopener noreferrer"><h2>{this.props.business.name}</h2></a>
         <div className="Business-information">
           <div className="Business-address">
-            <a href="https://www.google.com/maps/search/?api=1&query={this.props.business.name}" target="_blank" rel="noopener noreferrer"><p>{this.props.business.address}</p></a>
-            <p>{this.props.business.city}</p>
-            <p>{`${this.props.business.state} ${this.props.business.zipCode}`}</p>
+            <a href={`https://www.google.com/maps/search/?api=1&query=${this.props.business.address}, ${this.props.business.city}, ${this.props.business.zipCode}`} target="_blank" rel="noopener noreferrer"><p>{this.props.business.address}</p></a>
+            <a href={`https://www.google.com/maps/search/?api=1&query=${this.props.business.address}, ${this.props.business.city}, ${this.props.business.zipCode}`} target="_blank" rel="noopener noreferrer"><p>{this.props.business.city}</p></a>
+            <a href={`https://www.google.com/maps/search/?api=1&query=${this.props.business.address}, ${this.props.business.city}, ${this.props.business.zipCode}`} target="_blank" rel="noopener noreferrer"><p>{this.props.business.zipCode}</p></a>
           </div>
           <div className="Business-reviews">
             <h3>{this.props.business.category.toUpperCase()}</h3>
